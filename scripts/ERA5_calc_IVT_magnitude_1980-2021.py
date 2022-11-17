0f900e523599aa08237c2fa8b227aa5ff1b3d3f7
Kyle Mattingly <kyle.mattingly842@gmail.com>
2022-11-17 18:17:42 +0000

    minor typo fixes and changes to example config file

--- scripts/ERA5_calc_IVT_magnitude_1980-2021.py
+++ scripts/ERA5_calc_IVT_magnitude_1980-2021.py
@@ -17,6 +17,6 @@ for file_t in output_file_times:
     file_begin_t = dt.datetime(file_t.year, file_t.month, 1, 0, 0).strftime('%Y-%m-%d_%H%M')
     file_end_t = dt.datetime(file_t.year, file_t.month, file_t.day, 23, 0).strftime('%Y-%m-%d_%H%M')
     
-    os.system('python ../AR_alg_v2/calc_IVT_ERA5.py {file_begin_t} {file_end_t} {timestep_hrs}')
+    os.system(f'python ../AR_alg_v2/calc_IVT_ERA5.py {file_begin_t} {file_end_t} {timestep_hrs}')
     
     print('Finished writing '+str(file_t)+' at '+str(dt.datetime.now()))
