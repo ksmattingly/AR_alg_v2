92a1e6dbe14b00c1ab89da2dab8a31e675985e78
Kyle Mattingly <kyle.mattingly842@gmail.com>
2023-03-09 23:24:31 +0000

    write ERA5 IVT magnitude to a separate output file

--- AR_alg_v2/calc_IVT_magnitude_ERA5.py
+++ AR_alg_v2/calc_IVT_magnitude_ERA5.py
@@ -16,6 +16,7 @@ import numpy as np
 import datetime as dt
 import glob
 import warnings
+import os
 
 warnings.simplefilter('ignore', category=RuntimeWarning)
 
@@ -32,37 +33,84 @@ def calc_IVT_from_uvIVT(AR_config, begin_t_str, end_t_str, timestep_hrs_str):
     times = pd.date_range(begin_t, end_t, freq=str(timestep_hrs)+'H')
     file_t = pd.date_range(begin_t, end_t, freq=AR_config['uvIVT_precalc_files_timestep'])
     
-    uvIVT_fpath = glob.glob(AR_config['IVT_dir']+\
+    uvIVT_fpath = glob.glob(AR_config['uvIVT_precalc_dir']+\
                             AR_config['uvIVT_precalc_fname_prefix']+\
                             '*'+file_t[0].strftime(AR_config['uvIVT_precalc_fname_date_format'])+'*.nc')[0]
     uvIVT_ds = xr.open_dataset(uvIVT_fpath, mask_and_scale=True)
+    lats = uvIVT_ds.latitude.data
+    lons = uvIVT_ds.longitude.data
+    
+    uIVT = np.empty((len(times), uvIVT_ds.latitude.shape[0], uvIVT_ds.longitude.shape[0]), dtype='float32')
+    vIVT = np.empty((len(times), uvIVT_ds.latitude.shape[0], uvIVT_ds.longitude.shape[0]), dtype='float32')
     IVT = np.empty((len(times), uvIVT_ds.latitude.shape[0], uvIVT_ds.longitude.shape[0]), dtype='float32')
+    
     for i, t in enumerate(times):
         # Assumes we are calculating over the full lat/lon grid of the original file
         uvIVT_ds_ts = uvIVT_ds.sel(time=t)
         uIVT_ts = uvIVT_ds_ts['p71.162']
         vIVT_ts = uvIVT_ds_ts['p72.162']
         IVT_ts = np.sqrt((uIVT_ts**2) + (vIVT_ts**2))
+        
+        uIVT[i,:,:] = uIVT_ts
+        vIVT[i,:,:] = vIVT_ts
         IVT[i,:,:] = IVT_ts
     uvIVT_ds.close()
     
-    return uvIVT_fpath, times, IVT
+    return uvIVT_fpath, times, uIVT, vIVT, IVT, lats, lons
 
 
-def write_IVT_output_file(AR_config, timestep_hrs_str, uvIVT_fpath, times, IVT):
+def write_IVT_output_file(AR_config, timestep_hrs_str, uvIVT_fpath, times,
+                          uIVT, vIVT, IVT, lats, lons):
     """
-    Add IVT magnitude to the ERA5 uvIVT files.
+    Write files including IVT magnitude to the same directory as the input ERA5
+    uvIVT files.
     """
+    output_fname = os.path.basename(uvIVT_fpath)
+    output_fpath = AR_config['IVT_dir']+output_fname
     
-    with Dataset(uvIVT_fpath, 'r+') as nc:
-        IVT_out = nc.createVariable('IVT', 'float32', ('time','latitude','longitude',))
-        IVT_out.units = 'kg m**-1 s**-1'
-        IVT_out.long_name = 'Magnitude of vertically integrated water vapor transport'
-        IVT_out[:] = IVT
+    IVT_ds = xr.Dataset(
+        {
+         'uIVT':(('time','lat','lon'), uIVT),
+         'vIVT':(('time','lat','lon'), vIVT),
+         'IVT':(('time','lat','lon'), IVT)
+        },
+        coords={
+            'time':times,
+            'lat':lats,
+            'lon':lons
+        }
+    )
+    
+    encoding = {
+        'time':{'units':'hours since 1900-01-01', 'calendar': 'gregorian'},
+    }
+
+    IVT_ds.uIVT.attrs['units'] = 'kg/m/s'
+    IVT_ds.vIVT.attrs['units'] = 'kg/m/s'
+    IVT_ds.IVT.attrs['units'] = 'kg/m/s'
+    IVT_ds.lat.attrs['units'] = 'degrees_north'
+    IVT_ds.lon.attrs['units'] = 'degrees_east'  
+    
+    IVT_ds.attrs['data_source'] = AR_config['data_source']
+    IVT_ds.attrs['IVT_data_origin'] = AR_config['IVT_data_origin']
+    IVT_ds.attrs['IVT_vert_coord'] = AR_config['IVT_vert_coord']
+    
+    if AR_config['IVT_vert_coord'] == 'pressure_levels':
+        IVT_ds.attrs['IVT_calc_pressure_levels'] = str(AR_config['IVT_calc_plevs'])
+    elif AR_config['IVT_vert_coord'] == 'model_levels':
+        IVT_ds.attrs['IVT_calc_model_levels'] = str(AR_config['IVT_calc_mlevs'])
+
+    IVT_ds.to_netcdf(output_fpath, encoding=encoding)
+
+    # with Dataset(uvIVT_fpath, 'r+') as nc:
+    #     IVT_out = nc.createVariable('IVT', 'float32', ('time','latitude','longitude',))
+    #     IVT_out.units = 'kg m**-1 s**-1'
+    #     IVT_out.long_name = 'Magnitude of vertically integrated water vapor transport'
+    #     IVT_out[:] = IVT
         
-        nc.setncattr('data_source', AR_config['data_source'])
-        nc.setncattr('IVT_data_origin', AR_config['IVT_data_origin'])
-        nc.setncattr('IVT_vert_coord', AR_config['IVT_vert_coord'])
+    #     nc.setncattr('data_source', AR_config['data_source'])
+    #     nc.setncattr('IVT_data_origin', AR_config['IVT_data_origin'])
+    #     nc.setncattr('IVT_vert_coord', AR_config['IVT_vert_coord'])
         
 
 def parse_args():
@@ -86,8 +134,12 @@ def main():
     with open(AR_ID_config_path) as f:
         AR_config = hjson.loads(f.read())
     
-    uvIVT_fname, times, IVT = calc_IVT_from_uvIVT(AR_config, begin_t_str, end_t_str, timestep_hrs_str)
-    write_IVT_output_file(AR_config, timestep_hrs_str, uvIVT_fname, times, IVT)
+    # uvIVT_fname, times, IVT = calc_IVT_from_uvIVT(AR_config, begin_t_str, end_t_str, timestep_hrs_str)
+    uvIVT_fpath, times, uIVT, vIVT, IVT, lats, lons = \
+        calc_IVT_from_uvIVT(AR_config, begin_t_str, end_t_str, timestep_hrs_str)
+    # write_IVT_output_file(AR_config, timestep_hrs_str, uvIVT_fname, times, IVT)
+    write_IVT_output_file(AR_config, timestep_hrs_str, uvIVT_fpath, times,
+                          uIVT, vIVT, IVT, lats, lons)
 
 
 if __name__ == '__main__':
--- AR_alg_v2/misc_utils.py
+++ AR_alg_v2/misc_utils.py
@@ -27,10 +27,10 @@ def rename_IVT_components(ds):
     """
     Make sure u- and v-IVT components in xarray dataset are named "uIVT" and "vIVT".
     
-    (ERA5 pre-calculated uIVT and vIVT are named "" and "". Most other datasets
-    will have variables named "uIVT" and "vIVT" as needed for AR ID script,
-    because these variables are calculated "in house" by calc_IVT.py rather than
-    being provided in the original dataset.)
+    (ERA5 pre-calculated uIVT and vIVT are named "p71.162" and "p72.162". Most
+    other datasets will have variables named "uIVT" and "vIVT" as needed for
+    AR ID script, because these variables are calculated "in house" by calc_IVT.py
+    rather than being provided in the original dataset.)
     """
     
     if ('p71.162') and ('p72.162') in ds.variables:
